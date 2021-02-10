350556c926ad146a02516f0f5a858108ea4685fc
iyeshag <72629677+iyesha-Gonzalez@users.noreply.github.com>
2021-02-09 23:23:04 -0800

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,38 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.net.wifi.p2p.WifiP2pManager;
+
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.facebook.stetho.server.LeakyBufferedInputStream;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+
+    public String body;
+    public String createdAt;
+    public long id;
+    public User user;
+
+    public static  Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.id = jsonObject.getLong("id");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++){
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
+    }
+}
